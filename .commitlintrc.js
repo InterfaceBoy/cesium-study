1db28246757a62b6551700252a7aedd4899e4b69
何元鹏 <heyuanpeng@heyuanpengdeMacBook-Pro.local>
2024-04-01 11:24:40 +0800

    feat: :sparkles: 1
    1
    
    BREAKING CHANGE: 1

---
+++ .commitlintrc.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: 何元鹏
+ * @Date: 2024-04-01 11:16:51
+ * @LastEditors: 何元鹏
+ * @LastEditTime: 2024-04-01 11:17:11
+ */
+module.exports = {
+  extends: ["cz"],
+  rules: {
+      'type-empty': [2, 'never'],
+      'scope-enum': [
+    0
+  ]
+  },
+  prompt: {
+    useEmoji: true 
+  }
+};
